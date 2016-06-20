2cdd4aa0792cb52c82be1ce453595ac1c1aa5b87
Tomasz Polański <polanski.tomek@gmail.com>
2016-06-20 19:19:19 +0200

    Using kotlin in tests only

---
+++ options/core/src/test/kotlin/UnitTest.kt
@@ -0,0 +1,27 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import polanski.option.Unit.*
+import polanski.option.function.Action0
+
+class UnitTest {
+    @Test
+    fun testFrom_returnsUnit() {
+        assertThat(from(mock(Action0::class.java))).isEqualTo(DEFAULT)
+    }
+
+    @Test
+    fun testFrom_callsAction() {
+        val action = mock(Action0::class.java)
+
+        from(action)
+
+        Mockito.verify(action).call()
+    }
+
+    @Test
+    fun testAsUnit_returnsUnit() {
+        assertThat(asUnit(mock(Any::class.java))).isEqualTo(DEFAULT)
+    }
+}
